c340da36d3045d53b39ade519c06b17e71dae560
longph <longphamduy2002@gmail.com>
2023-01-19 01:26:15 +0200

    project restructure

---
+++ src/routes/fact/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+      /** @type {import('./$types').PageServerData} */
+    export let data;
+    console.log(data);    
+</script>
+
+<ul>
+    {#each data.facts as fact}
+        <lI>{fact.fact}</lI>
+    {/each}
+</ul>
