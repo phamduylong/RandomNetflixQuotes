2a38d79ff7cf52f885395f6cb6d8b42e9e911830
longph <longphamduy2002@gmail.com>
2023-01-19 02:52:34 +0200

    add new route for random category

--- src/routes/fact/+page.server.js
+++ src/routes/fact/+page.server.js
@@ -3,7 +3,7 @@ import { API_KEY } from '$env/static/private'
 export async function load ({data, fetch}) {
     const response = await fetch(`https://api.api-ninjas.com/v1/facts?limit=30`, {
         headers: {
-            'X-Api-Key': API_KEY
+            'X-Api-Key': ''
         }
     });
 
--- src/routes/fact/+page.svelte
+++ src/routes/fact/+page.svelte
@@ -3,7 +3,7 @@
     export let data;
     console.log(data);    
 </script>
-
+This is fact page
 <ul>
     {#each data.facts as fact}
         <lI>{fact.fact}</lI>
