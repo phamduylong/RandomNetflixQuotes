5449edcdaa0200c56ecc7b2fef7bdadf5b3fe445
longph <longphamduy2002@gmail.com>
2023-01-19 02:53:19 +0200

    add layout page with header

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let title= "Random Generator"
+</script>
+
+<header>{title}</header>
+
+<slot/>
+
+<style>
+    header {
+        font-size: 3vw;
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        margin-top: 1%;
+        margin-bottom: 5%;
+    }
+</style>
