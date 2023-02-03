99f997cdbf73f9f5a6e4545c300fdb724deffc58
longph <longphamduy2002@gmail.com>
2023-02-03 19:29:57 +0200

    add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import "../app.css";
 </script>
 
 <div id="navbar">
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,8 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
