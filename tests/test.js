6759b5dbfe643e64df887ba05f6eef78eadcbbf8
longph <longphamduy2002@gmail.com>
2023-03-13 19:52:55 +0200

    add basic title testcases

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,23 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test("Page Title Test", async ({ page }) => {
+  // Random Page Title
+  await page.goto("/");
+  await expect(page).toHaveTitle("Quotes");
+
+  // Stranger Things Page Title
+  await page.goto("/Stranger%20Things");
+  await expect(page).toHaveTitle("Stranger Things Quotes");
+
+  // Breaking Bad Page Title
+  await page.goto("/Breaking%20Bad");
+  await expect(page).toHaveTitle("Breaking Bad Quotes");
+
+  // Better Call Saul Page Title
+  await page.goto("/Better%20Call%20Saul");
+  await expect(page).toHaveTitle("Better Call Saul Quotes");
+
+  // Lucifer Page Title
+  await page.goto("/Lucifer");
+  await expect(page).toHaveTitle("Lucifer Quotes");
 });
