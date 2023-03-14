cbb81f9efbd9d97ac1f15e57b1f16ba004570bd0
longph <longphamduy2002@gmail.com>
2023-03-14 08:46:18 +0200

    add clipboard permission

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,12 @@ const config = {
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'tests'
+	testDir: 'tests',
+	use: {
+		browserName: "chromium",
+		headless: false,
+		permissions: ["clipboard-read"],
+	}
 };
 
 export default config;
